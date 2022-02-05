4404ca2ed7b899ae8575ebbb3a28f9c6df8fbad1
anthonymatignon <anthony.matignon@gmail.com>
2022-02-05 12:16:21 +0100

    Udate podcasts file names

--- src/mock/podcasts.ts
+++ src/mock/podcasts.ts
@@ -1,14 +1,14 @@
 export default [
     {
         "title":"Afterlife Voyage 001 by Tale Of Us",
-        "picture": "afterlife/podcasts/voyage-001.jpeg",
+        "picture": "afterlife/podcasts/VOYAGE-001.jpeg",
         "duration": 100,
         "soundcloudUrl": "https://soundcloud.com/taleofus/afterlife-voyage-001",
         "date": "2016-01-01",
     },
     {
         "title":"Afterlife Voyage 002 by Vaal",
-        "picture": "afterlife/podcasts/voyage-002.jpeg",
+        "picture": "afterlife/podcasts/VOYAGE-002.jpeg",
         "duration": 100,
         "soundcloudUrl": "https://soundcloud.com/afterlifeofc/afterlife-voyage-002-by-vaal",
         "date": "2016-01-01",
