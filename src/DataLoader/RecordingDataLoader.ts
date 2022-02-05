86a28c98d8e176f38e200f62c1df5a1bf7b9370e
anthonymatignon <anthony.matignon@gmail.com>
2022-02-05 12:11:11 +0100

    Add recordings and rationalize all picture names

--- src/DataLoader/RecordingDataLoader.ts
+++ src/DataLoader/RecordingDataLoader.ts
@@ -1,13 +1,28 @@
+import recordings from "../mock/recordings"
+
+enum SocialMediaLinkType {
+    Spotify = "SPOTIFY",
+    Beatport = "BEATPORT",
+    Itunes = "ITUNES",
+    Vinyl = "VINYL"
+}
+
+interface SocialMediaLink {
+    type: string|SocialMediaLinkType
+    link: string
+}
 
 interface Recording {
     sku: string
     title: string
     picture: string
     content: string
-    socialMediaLinks: Array<string>
+    socialMediaLinks: Array<SocialMediaLink>
 }
 
 export default class RecordingDataLoader
 {
-
+    findAll(): Array<Recording> {
+        return recordings
+    }
 }
---
+++ src/mock/recordings.ts
@@ -0,0 +1,88 @@
+export default [
+    {
+        sku: "AL001",
+        title: "VA – Realm Of Consciousness",
+        picture: "/afterlife/recordings/AL001.jpeg",
+        content: "Afterlife and Tale Of Us are proud to announce ‘Realm Of Consciousness’, a 10-piece compilation expanding the Afterlife totality into a label. The label and events will complement each other with one  aim; to bring the crowd into another dimension of music perception with enthralling production to that ignite the senses.\n" +
+            "\n" +
+            "The ‘Realm Of Consciousness’ voyage commences with Tale Of Us’ Lies – a fitting introduction with quivering keys and restrained percussion that engenders an ominous suspense ahead of Monoloc’s enigmatic industrial techno crunch, Phoenix. Shifting through the haunting chords of Recondite’s dynamic Murphy’s Law, to the brooding synth of Poseidon by Ukrainian duo Woo York, the realisation of Afterlife utopia is upon us. The repetitive, mesmerising synth and effective reverb that Somne creates with Triangle is an odyssey within itself. From there, John Hester’s expertly woven ‘Logan’ is a feverish techno stab that transverses through the emotional senses, transitioning to Locked Groove’s adroitly crafted Emeralds, to Ryan James Ford’s ambient, albeit percussive, techno cut Toree Bahrstad. Die Wiederkehr is the outcome of Obscure Shape & SHDW’s collaboration, which incorporates facets of techno and trance that have been shaped with real guile and finesse. Mind Against round off the compilation with Gravity – a mystifying ambience and a fitting title as we touch back down to planet earth.",
+        socialMediaLinks: [
+            {
+            type: "SPOTIFY",
+            link: "https://play.spotify.com/album/3JhFuFkXfTCjXj4Uh6gx2w?play=true"
+            },
+            {
+                type: "BEATPORT",
+                link: "https://www.beatport.com/release/realm-of-consciousness/1788905"
+            },
+            {
+                type: "ITUNES",
+                link: "https://itunes.apple.com/au/album/realm-of-consciousness/id1119758918"
+            },
+            {
+                type: "VINYL",
+                link: "https://shop.after.life/collections/records"
+            }
+        ],
+    },
+    {
+        sku: "AL002",
+        title: "Vaal – Wander To Hell",
+        picture: "/afterlife/recordings/AL001.jpeg",
+        content: "The next installment on Afterlife comes from Vaal with remixes by Locked Groove and Baikal. ’Wander To Hell’ is a three track EP, with three versions of the same track that each encapsulate the spirit of Afterlife in their own individual way.\n" +
+            "\n" +
+            "Heralding from a rock n’roll and jazz background Vaal is unperturbed by the strict rules that define & guard electronic music. ‘Wander To Hell’ is stripped back and gentle, highlighting the emotive side of the label. The combination of alluring vocals and beautiful reverberating chords along with the ebb and flow of distortion makes it a composition that grips the listener from start to finish.\n" +
+            "\n" +
+            "Belgian talent Locked Groove seeks the dancefloor with his remix. He brings back a little familiarity, the vocal remains, but is now present amongst an assortment of synths and percussion, pushing through to darker, heavier depths.\n" +
+            "\n" +
+            "Baikal makes multi layered electronic music simultaneously for the heart, mind and feet. His version of ‘Wander To Hell’ is truly spellbinding, featuring a hypnotic groove that glides seamlessly through a continuum of time and space.",
+        socialMediaLinks: [
+            {
+                type: "SPOTIFY",
+                link: "https://open.spotify.com/album/0L4RALk62NPYoR6hw8Xv8f"
+            },
+            {
+                type: "BEATPORT",
+                link: "https://www.beatport.com/release/wander-to-hell/1878473"
+            },
+            {
+                type: "ITUNES",
+                link: "https://itunes.apple.com/album/wander-to-hell-single/id1168030762?l=de&ls=1"
+            },
+            {
+                type: "VINYL",
+                link: "https://shop.after.life/collections/records"
+            }
+        ],
+    },
+    {
+        sku: "AL003",
+        title: "Mind Against & Aether – Solaris",
+        picture: "/afterlife/recordings/AL003.jpeg",
+        content: "Filled with contrasts in light and shadow and all the shades in between, Afterlife is thrilled to announce a collaborative release of Mind Against and Aether. ‘Solaris’ is a melancholic listen in the form of a three track EP that brings in the light, dives in the dark and finally grounds you.\n" +
+            "\n" +
+            "The title track ‘Solaris’ starts off with a poised, hypnotic groove that surges into arpeggiated chords, lighting the way. ‘Eclipse’ drifts off into a svelte bassline working with a darker, driven composition of synths. ‘Event Horizon’ finishes with an immaculately hewn journey, with focus on harmonic developments and whirring polymetrics.\n" +
+            "\n" +
+            "A1. Solaris\n" +
+            "B1. Eclipse\n" +
+            "B2. Event Horizon",
+        socialMediaLinks: [
+            {
+                type: "SPOTIFY",
+                link: "https://open.spotify.com/album/7KPGIirUj8LCjGUTnXpCgB",
+            },
+            {
+                type: "BEATPORT",
+                link: "https://www.beatport.com/release/solaris-ep/1937437",
+            },
+            {
+                type: "ITUNES",
+                link: "https://itunes.apple.com/us/album/solaris-single/id1197517511",
+            },
+            {
+                type: "VINYL",
+                link: "https://shop.after.life/collections/records",
+            },
+        ],
+    },
+]
